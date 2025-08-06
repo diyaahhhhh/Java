33a59f690cedbf9021a70845d01b225ff0f6053d
𝘿𝙄𝙔𝘼🪐 <diyannjoseph2006@gmail.com>
2025-08-06 10:07:29 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package lab1;
+
+class Student{
+	String name;
+	static int studentCount=0;
+	Student(String name){
+		this.name=name;
+		studentCount++;
+	}
+	public static int getStudentCount(){
+		return studentCount;
+}
+}
+
+public class Main {
+	public static void main(String[] args) {
+         Student s1=new Student("Annu");
+         System.out.println(Student.getStudentCount());
+         Student s2=new Student("Sona");
+         System.out.println(Student.getStudentCount());
+         Student s3=new Student("Rahul"); 
+         System.out.println(Student.getStudentCount());
+    }
+}
